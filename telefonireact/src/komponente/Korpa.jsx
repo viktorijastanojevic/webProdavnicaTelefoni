bf2ad9cd8505ef64455fcdcab1b33258b6e24671
aleksRistic97 <ar20180420@student.fon.bg.ac.rs>
2022-05-26 17:44:28 +0200

    omoguceno generisanje pdf racuna

--- telefonireact/src/komponente/Korpa.jsx
+++ telefonireact/src/komponente/Korpa.jsx
@@ -1,8 +1,35 @@
- 
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 import React from "react";
 import {BsPlusLg, BsDashLg} from "react-icons/bs"
 const Korpa = ({products, sum,onAdd,onRemove}) => {
-
+    function exportPDF() {
+        const unit = "pt";
+        const size = "A4"; // Use A1, A2, A3 or A4
+        const orientation = "landscape"; // portrait or landscape
+    
+        const marginLeft = 40;
+        const doc = new jsPDF(orientation, unit, size);
+    
+        doc.setFontSize(15);
+        var today = new Date();
+        const footer = "\t\t\t\t\t\t\t\t\t\t\t\tUKUPNO ZA UPLATU: "+sum;
+        const title = "Racun na dan: "+  today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()+footer;
+        const headers = [["MODEL", "CENA","KOLICINA","UKUPNO"]];
+        console.log(products)
+        const data = products.map(elt=> [elt.name, elt.price, elt.amount, elt.price*elt.amount]);
+      
+        let content = {
+          startY: 50,
+          head: headers,
+          body: data ,
+          
+        };
+    
+        doc.text(title, marginLeft, 40);
+        doc.autoTable(content);
+        doc.save("racun.pdf")
+      }
  
     return (
       
@@ -10,7 +37,7 @@ const Korpa = ({products, sum,onAdd,onRemove}) => {
           <br /><br /><br />
           <table className="tabelaKorpa">
             <tbody >
-                    <tr><th>Model</th><th>Cena</th><th>Kolicina</th><th>Ukupno</th></tr>
+                    <tr><th>Model</th><th>Cena</th><th>Kolicina</th><th>Ukupno</th><th></th></tr>
 
                 {products.map((prod)=> (
                     <tr key={prod.id}><th>{prod.name}</th><td><b>{prod.price}</b></td><td><b>{prod.amount}</b></td><td><b>{prod.amount*prod.price}</b></td><td><button className="btn"
@@ -19,11 +46,14 @@ const Korpa = ({products, sum,onAdd,onRemove}) => {
             </tbody>
         </table>
         <br /><br /><br />
-      <div className="cenaDiv"><h3>Ukupna cena proizvoda: {sum} RSD</h3>
-          </div>
+        <div className="cenaDiv"><h3>Ukupna cena proizvoda: {sum} RSD</h3>
+        </div>
+        <div>
+        <button className="btn btn--pill btn--green" onClick={() => exportPDF()}>Generisi racun</button>
+        </div>
       </div>
 
-      
+
     );
   };
 
