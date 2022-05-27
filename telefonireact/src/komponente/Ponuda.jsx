7a8222db13be44fd54f4d2fb6cee0b5bb4b73e65
aleksRistic97 <ar20180420@student.fon.bg.ac.rs>
2022-05-27 18:59:07 +0200

    omoguceno sortiranje proizvoda po ceni

--- telefonireact/src/komponente/Ponuda.jsx
+++ telefonireact/src/komponente/Ponuda.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TelefonKartica from './TelefonKartica'
 
 function Ponuda({telefoni,dodajTelefon,onAdd,onRemove }) {
+  const [sort, setSort] = useState(true);
+  function sortAsc(){
+    
+    setSort(true);
+  }
+  function sortDesc(){
+    
+    setSort(false);
+  }
   return (
-    <div className='sviTelefoni'>
-           
-
-      
-           {telefoni.map((t)=>(<TelefonKartica key={t.id} product={t} dodajTelefon={dodajTelefon}  onAdd={onAdd} onRemove={onRemove} inCart={1}></TelefonKartica>))}
+    <div>
+        <button className="sortbtn btn" onClick={sortAsc}>Sortiraj rastuće</button>
+        <button className="sortbtn btn" onClick={sortDesc}>Sortiraj opadajuće</button>
+        <div className='sviTelefoni'>
+              
+            {sort===true?
+              <>
+                {telefoni
+                     .sort((a, b) => a.price < b.price ? -1 : 1)
+                    .map((t)=>(<TelefonKartica key={t.id} product={t} dodajTelefon={dodajTelefon}  onAdd={onAdd} onRemove={onRemove} inCart={1}></TelefonKartica>))}
+              </>
+              :
+              <>
+                {telefoni
+                     .sort((a, b) => a.price > b.price ? -1 : 1)
+                    .map((t)=>(<TelefonKartica key={t.id} product={t} dodajTelefon={dodajTelefon}  onAdd={onAdd} onRemove={onRemove} inCart={1}></TelefonKartica>))}
+              </>
+            }
           
-           
+              
+              
+              
 
+        </div>
     </div>
   )
 }
